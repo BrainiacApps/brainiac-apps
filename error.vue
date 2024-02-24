c2781df071079ece4e08aa872d22a8653929c3a3
elenaortega <elenaorfe@gmail.com>
2024-02-24 20:29:35 +0100

    Add custom error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <section class="container mx-auto px-4">
+      <div class="flex flex-col gap-16 justify-center items-center h-screen">
+        <p>
+          Oops! This page doesn't exist. Let's get you back on track – hit the
+          back button.
+        </p>
+        <Button :handleClick="handleError" text="Back" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+const handleError = () => clearError({ redirect: "/" });
+</script>
